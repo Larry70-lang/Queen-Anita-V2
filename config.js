2d339a6faf677efcb5e2f9e9ae268685e6c5dbfb
Larry70-lang <donhillary06@gmail.com>
2024-10-10 11:40:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3AD
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
 global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.owner = process.env.OWNER_NUMBER || "2349153537191";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -34,7 +34,7 @@ global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
 global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
-global.save_status_from = process.env.SAVE_STATUS_FROM || "";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "true";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
 global.api_smd = "https://api-smd-1.vercel.app";
@@ -42,7 +42,7 @@ global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0I1ejYxaitjMnRXbEhUS2Fhb3N4MkpvNTUxUjBNZEU4NnlZQWlPN0EwZz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTE11ejUxZFIxYy9VcE10Tmp0bHpTeXpzZnU5TlpacmxFKzh3RmhlV1Z5az0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJtS2tudDZuaXRmZVFUYTVQQ1FkV25ta1JVdmZiZ1o3R2ZEcXBVZ1U3bVZZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJxZHZNNE1RQTVLQ1psaHpheForWVNaMnM1VTJtL3dRZ1VIcC9vVGVxK3dRPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhON2JIODRmT3A4VjZETWZDMzB3M25oeGN0ZTBwdWIvaGQ0Q3QzaXA0Mms9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkQ3ZHFqSnIzNGFmcEVhOW9MOFVPOWJEQVN3RDI4YjRlOFNrL3hmVHV0d2s9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZUZQMTV6MldmN0ZjM29jQU1INUVKWGdFc2lYRittaHY3VnFCMlpnaDduST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ3hoSU5Cd3RCSjRQVGxlNktDZXFZeXJFalVkSTlGcS9UZlF4aXB3K3Fobz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFieE9ZcldiaU04czV3enhsbXB3MHZqMUx4ZGVtY2w2a3czSU90UDdDZks4MU4vaWovbkhMaXNnaUg2MUtIOXB0QU8wbWdsYnMzS1VFREZwSGVEcml3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MSwiYWR2U2VjcmV0S2V5IjoiUmJxOWV6UWx4TWtBRU5sREduK3BscTFOOTd5akVqbXNMZ3lBUjlpaEJjTT0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoiU0N1ZUxjSGFTd1dCWnRIOWhOQmJKQSIsInBob25lSWQiOiJiOTZjNzU3Yi1iMWYzLTQzMGMtYmY2MC05Nzg4YzBhMjVmMGQiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicFUyQXQxR0M4LzZPNXFVQ3Q2WEU4OHkrWVJjPSJ9LCJyZWdpc3RlcmVkIjp0cnVlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlptWk5MSXRTOVdpOU4vcSt3azZUbm9kbllZUT0ifSwicmVnaXN0cmF0aW9uIjp7fSwicGFpcmluZ0NvZGUiOiJENEVQQ1c4NCIsIm1lIjp7ImlkIjoiMjM0OTE1MzUzNzE5MToyMEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSlNUNXRjR0VPdlpucmdHR0JjZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiL1JGdjZjVERLVnZXbkxQcTBjMlR4OVI5MWs5SlV1R3ZXUGdYMHlJTHhYOD0iLCJhY2NvdW50U2lnbmF0dXJlIjoiVGxKY2Jpb1BMSHkvcUhCdjlSSVp3aGg5Mi83RUM4ZXVHLzRrOXFHaUZHemJ1bkd1cmlEZ0I3NnUvc2VGRkIwN0tuNkxOSENueGVOdURSRWloUmVZQVE9PSIsImRldmljZVNpZ25hdHVyZSI6IisrcGhoZEh0bFBWdEsxc3IwTUlEUFQxZTh2MG9sRXNmY0NOaGxtU3B2YXdoVEVDdm9Ja3U3SmZmbWJKeU9yQ2FTWTFhZzJYa25rSUZ5QXVGQTZOa2dnPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjM0OTE1MzUzNzE5MToyMEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJmMFJiK25Fd3lsYjFweXo2dEhOazhmVWZkWlBTVkxocjFqNEY5TWlDOFYvIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzI4NTU2MjgxLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUlQeiJ9"
 module.exports = {
   menu: process.env.MENU || "2",
   HANDLERS: process.env.PREFIX || ".",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
   botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  ownername: process.env.OWNER_NAME || "Hillarylarry",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
